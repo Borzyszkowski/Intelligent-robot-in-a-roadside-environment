b72f8a5eaaac1787379924e165076fac2b090a3d
Bartek <bartek_borzyszkowski@wp.pl>
2019-04-13 21:19:48 +0200

    Test programs

--- Tests/Raspbbery Pi - internal/Camera/camera_raspberry.py
+++ Tests/Raspbbery Pi - internal/Camera/camera_raspberry.py
@@ -5,11 +5,10 @@ __author__ = 'Borzyszkowski'
 from picamera import PiCamera
 from time import sleep
 
-camera = PiCamera()
 
+camera = PiCamera()
 camera.rotation = 180
 camera.start_preview()
-
 camera.start_recording('./Recorded/video.h264')
 sleep(10)
 camera.stop_recording()
